ee002cef1bbda4dcf22de1b59ea3b89967864727
anthony ang <anthonyang25@gmail.com>
2019-01-17 00:27:08 +0800

    auto generate hashcode and equals method

--- learning-datastructures/src/com/anthonyang/ds/collections/SetDemo.java
+++ learning-datastructures/src/com/anthonyang/ds/collections/SetDemo.java
@@ -76,14 +76,50 @@ class Book {
 
 	@Override
 	public int hashCode() {
-		return title.hashCode(); //both obj will return same hashcode
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + year;
+		return result;
 	}
 
-	//overide equal method used by object class internally as it uses "==" thus obj ref will be different
 	@Override
 	public boolean equals(Object obj) {
-		return (year == (((Book)obj).getYear())) && (author.equals((((Book)obj).getAuthor())));
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
 	}
+
+//	@Override
+//	public int hashCode() {
+//		return title.hashCode(); //both obj will return same hashcode
+//	}
+//
+//	//overide equal method used by object class internally as it uses "==" thus obj ref will be different
+//	@Override
+//	public boolean equals(Object obj) {
+//		return (year == (((Book)obj).getYear())) && (author.equals((((Book)obj).getAuthor())));
+//	}
+//	
+	
 	
 	
 	
