3d637c2be47aba32a3c17a74b761b8ca07f5727c
anthony ang <anthonyang25@gmail.com>
2019-01-17 22:21:40 +0800

    Added iterating using KeySet and EntrySet

--- learning-datastructures/src/com/anthonyang/ds/collections/MapDemo.java
+++ learning-datastructures/src/com/anthonyang/ds/collections/MapDemo.java
@@ -2,23 +2,40 @@ package com.anthonyang.ds.collections;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapDemo {
-	
+
 	private static void hashMap() {
-		//programming to Map Interface.
-		Map<String, Integer> map1 = new HashMap<>(); 
+		// programming to Map Interface.
+		Map<String, Integer> map1 = new HashMap<>();
 		map1.put("James", 29);
 		map1.put("John", 27);
 		map1.put("Ken", null);
 		System.out.println(map1);
-		
-		map1.put("ken", 28);
+
+		map1.put("Ken", 28);
 		System.out.println(map1);
-		
+
 		System.out.println("Contains James <key>? " + map1.containsKey("James"));
 		System.out.println("James Age <value>: " + map1.get("James"));
+
+		System.out.println("--- Iterate using keySet ---");
+		Set<String> names = map1.keySet(); // returns a set of all keys in map
+		System.out.println(names);
+		for (String name : names) {
+			System.out.println(name + " " + map1.get(name));
+		}
+
+		System.out.println("--- Iterate using EntrySet ---");
+		Set<Map.Entry<String, Integer>> mappings = map1.entrySet(); //returns a set of map entry (k,v) pair
 		
+		for (Map.Entry<String, Integer> mapping : mappings) {
+			System.out.println("Name: " + mapping.getKey() + ", Age: " + mapping.getValue());
+		}
+
+		names.remove("Ken"); //removing the key from the new names Set will affect the original Map
+		System.out.println(map1);
 	}
 
 	public static void main(String[] args) {
