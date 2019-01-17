b1bb4f28f1a8771fafca4886131bf60a58d18ae6
anthony ang <anthonyang25@gmail.com>
2019-01-17 22:00:32 +0800

    Added some basic HashMap operations

--- learning-datastructures/src/com/anthonyang/ds/collections/MapDemo.java
+++ learning-datastructures/src/com/anthonyang/ds/collections/MapDemo.java
@@ -11,8 +11,14 @@ public class MapDemo {
 		map1.put("James", 29);
 		map1.put("John", 27);
 		map1.put("Ken", null);
-		
 		System.out.println(map1);
+		
+		map1.put("ken", 28);
+		System.out.println(map1);
+		
+		System.out.println("Contains James <key>? " + map1.containsKey("James"));
+		System.out.println("James Age <value>: " + map1.get("James"));
+		
 	}
 
 	public static void main(String[] args) {
