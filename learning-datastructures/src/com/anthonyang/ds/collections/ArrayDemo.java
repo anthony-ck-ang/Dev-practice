b2c6936f5400e0964746637dbf48a65cc62f885d
anthony ang <anthonyang25@gmail.com>
2019-01-18 13:34:42 +0800

    create arraydemo class and asList method

---
+++ learning-datastructures/src/com/anthonyang/ds/collections/ArrayDemo.java
@@ -0,0 +1,13 @@
+package com.anthonyang.ds.collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayDemo {
+	
+	String[] stringArray = new String[]{"aaa", "bbb"};
+	// asList() -> most commonly used
+	List<String> strings = Arrays.asList(stringArray); // fixed-size
+	
+
+}
