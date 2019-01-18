7ccadb83033733e4b6f953d14b445f2d8a85b16b
anthony ang <anthonyang25@gmail.com>
2019-01-19 01:59:01 +0800

    added awaitTermination and ShutdownNow method

--- learning/src/com/anthonyang/concurrency/SimpleExecutorServiceExample.java
+++ learning/src/com/anthonyang/concurrency/SimpleExecutorServiceExample.java
@@ -2,9 +2,10 @@ package com.anthonyang.concurrency;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SimpleExecutorServiceExample {
-	
+
 	public static void main(String[] args) {
 		// ref: https://dzone.com/articles/the-executor-framework
 
@@ -19,12 +20,24 @@ public class SimpleExecutorServiceExample {
 		 * up by worker thread and will get executed.
 		 */
 		for (int i = 0; i < 10; i++) {
-			Runnable worker = new WorkerThread("" + i); //using Threads
+			Runnable worker = new WorkerThread("" + i); // using Threads
 			executor.execute(worker);
 		}
-		executor.shutdown();
-		while (!executor.isTerminated()) {
-		}
+//		executor.shutdown(); // Initiates an orderly shutdown in which
+//								// previously submitted tasks are executed, but
+//								// no new tasks will be accepted.
+//		
+//		while (!executor.isTerminated()) {
+//			//keep looping until true
+//		}
+		 executor.shutdown();
+		 try {
+		 if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+		 executor.shutdownNow();
+		 }
+		 } catch (InterruptedException e) {
+		 executor.shutdownNow();
+		 }
 		System.out.println("Finished all threads");
 	}
 
@@ -41,7 +54,7 @@ class WorkerThread implements Runnable {
 	@Override
 	public void run() {
 		System.out.println(Thread.currentThread().getName() + " Start. Command = " + command);
-		processCommand();
+		//processCommand();
 		System.out.println(Thread.currentThread().getName() + " End.");
 	}
 
