9057fe0f6680d10a38c8f17c989e91a3bc1af84b
anthony ang <anthonyang25@gmail.com>
2019-01-18 13:44:51 +0800

    updated arraydemo class

--- learning-datastructures/src/com/anthonyang/ds/collections/ArrayDemo.java
+++ learning-datastructures/src/com/anthonyang/ds/collections/ArrayDemo.java
@@ -5,9 +5,15 @@ import java.util.List;
 
 public class ArrayDemo {
 	
-	String[] stringArray = new String[]{"aaa", "bbb"};
-	// asList() -> most commonly used
-	List<String> strings = Arrays.asList(stringArray); // fixed-size
-	
+	private static void ArraysDemo() {
+		String[] stringArray = new String[]{"aaa", "bbb"};
+		// asList() -> most commonly used
+		List<String> strings = Arrays.asList(stringArray); // fixed-size
 
+		System.out.println("strings: " + strings);		
+	}
+
+	public static void main(String[] args) {
+		ArraysDemo();
+	}
 }
