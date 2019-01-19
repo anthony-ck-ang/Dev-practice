3dc16292c3af173e38ddd05a2f6a02e37bc7d9e3
anthony ang <anthonyang25@gmail.com>
2019-01-19 18:14:46 +0800

    added lambda impl in FirstThread class

--- learning/src/com/anthonyang/concurrency/FirstThread.java
+++ learning/src/com/anthonyang/concurrency/FirstThread.java
@@ -6,6 +6,7 @@ public class FirstThread {
 
 	private static void firstThread() {
 		Task task = new Task();
+
 		Thread thread = new Thread(task); // add task to thread
 		thread.start();// Causes this thread to begin execution
 
@@ -17,12 +18,58 @@ public class FirstThread {
 		}
 	}
 
+	private static void firstThreadWithLambda() {
+
+		/*
+		 * Cannot predict if the runnable will be invoked before or after
+		 * printing 'done'. The order is non-deterministic.
+		 */
+		Runnable task = () -> {
+			System.out.println(Thread.currentThread().getName() + " thread");
+			// some task
+		};
+
+		task.run();
+
+		Thread thread = new Thread(task);
+		thread.start();
+
+		System.out.println("Done!");
+
+	}
+
+	private static void sleepThreadWithLambda() {
+		/*
+		 * Threads can be put to sleep for a certain duration -> to simulate
+		 * long running tasks
+		 */
+		Runnable sleep = () -> {
+
+			String name = Thread.currentThread().getName();
+			System.out.println("Thread Name: " + name);
+			try {
+				TimeUnit.SECONDS.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Thread Name: " + name);
+
+		};
+
+		Thread t = new Thread(sleep);
+		t.start();
+
+	}
+
 	public static void main(String[] args) {
-		firstThread();
-		
-		Thread myThread = new FirstThread2();
-		myThread.start();
+		// firstThread();
+		// firstThreadWithLambda();
+		sleepThreadWithLambda();
+
+		// Thread myThread = new FirstThread2();
+		// myThread.start();
 		System.out.println("Inside main ... main thread");
+
 	}
 
 }
@@ -41,7 +88,7 @@ class Task implements Runnable {
 
 }
 
-//another way, Thread also implements Runnable
+// another way, Thread also implements Runnable
 class FirstThread2 extends Thread {
 
 	public void run() {
