6a08f7cc440d71ee901332fde797675462b283ba
anthony ang <anthonyang25@gmail.com>
2019-01-20 23:37:57 +0800

    added groupByAge()

--- learning/src/com/anthonyang/functional/stream/Jav8AdvStreamDemo.java
+++ learning/src/com/anthonyang/functional/stream/Jav8AdvStreamDemo.java
@@ -3,13 +3,14 @@ package com.anthonyang.functional.stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Jav8AdvStreamDemo {
 
 	static ArrayList<Person> persons = new ArrayList<Person>(
-			Arrays.asList(new Person("a", 21), new Person("b", 27), new Person("c", 29), new Person("d", 8)));
+			Arrays.asList(new Person("a", 21), new Person("b", 27), new Person("c", 27), new Person("d", 8)));
 
 	/*
 	 * Collect is an extremely useful terminal operation to transform the
@@ -37,8 +38,21 @@ public class Jav8AdvStreamDemo {
 		
 	}
 	
+	private static void groupByAge(){
+		
+		Map<Integer, List<Person>> personsByAge = persons
+			    .stream()
+			    .collect(Collectors.groupingBy(p -> p.age));
+
+			personsByAge
+			    .forEach((age, p) -> System.out.format("age %s: %s\n", age, p));
+			
+			System.out.println(personsByAge); //age -> (Key), list of person -> (value), {21=[a], 8=[d], 27=[b, c]}
+	}
+	
 	public static void main(String[] args) {
-		filterAndCollect();
+		//filterAndCollect();
+		groupByAge();
 	}
 
 }
